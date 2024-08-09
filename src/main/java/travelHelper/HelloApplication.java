62e5107a8098c79a9416a82e6ee70db85a93383b
milanKosanovic <mkosanovic5820rn@raf.rs>
2024-08-09 13:26:23 +0200

    Entities added successfully!

--- src/main/java/travelHelper/HelloApplication.java
+++ src/main/java/travelHelper/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.domacirest;
+package travelHelper;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- src/main/java/travelHelper/HelloResource.java
+++ src/main/java/travelHelper/HelloResource.java
@@ -1,4 +1,4 @@
-package com.example.domacirest;
+package travelHelper;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
